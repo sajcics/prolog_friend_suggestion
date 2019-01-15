89c9da5b7fc661172d712a0739bdb6d198140d0e
ss <ssajcic@tria.hr>
2019-01-15 11:04:12 +0100

    init project

---
+++ prolog/index.py
@@ -0,0 +1,14 @@
+import logging
+import pyxf
+#x = pyxf.xsb('../static/XSB/bin')
+
+log = logging.getLogger('prolog')
+
+def callProlog():
+    log.error("inside callProlog()")
+#    x.load('osobe')
+#    res = x.query('prijatelj_prijatelja(\'roberto\', X)')
+#    print('Iz XSB:')
+#    for r in res:
+#        print(r['X'])
+#    del x
